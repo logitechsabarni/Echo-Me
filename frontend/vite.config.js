2b2797cdd88b4ca209ffc418a9c948a7eb2a16ef
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-26 01:19:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    define: {
+      'process.env': env, // Makes Vite environment variables available
+    }
+  };
 });
